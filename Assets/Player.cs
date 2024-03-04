c04419bb413cf4a69340cbbc7bc9875b04be0413
logzinga <65839350+logzinga@users.noreply.github.com>
2024-03-04 11:09:07 +1100

    basic player controls

---
+++ Assets/Player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+
+    public float PlayersMovementSpeed;
+    private float _playersMovementDirection = 0;
+    private Controls _inputActionReference;
+    private Rigidbody2D _playersRigidBody;
+
+    private void Start() {
+        _playersRigidBody ??= GetComponent<Rigidbody2D>();
+
+        _inputActionReference = new Controls();
+        _inputActionReference.Enable();
+
+        _inputActionReference.Ground.Move.performed += moving => {
+            _playersMovementDirection = moving.ReadValue<float>();
+        };
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        _playersRigidBody.velocity = new Vector2(_playersMovementDirection * PlayersMovementSpeed, _playersRigidBody.velocity.y);
+    }
+}
